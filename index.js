604d0ecf40278ffbee45d29f66becd44229d6ab9
agurusa <agurusa@gmail.com>
2018-04-22 16:53:40 -0700

    Add svg capability from d3

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-var dataset = [ 5, 10, 12, 20, 14, 3 ];
+var dataset = [ 25, 7, 5, 26, 11, 8, 25, 14, 23, 19,
+                14, 11, 22, 29, 11, 13, 12, 17, 18, 10,
+                24, 18, 25, 9, 3 ];
 
+var w = 500;
+var h = 100;
+var barPadding = 1;
 
-d3.select("body").selectAll("div")
-    .data(dataset)
-    .enter()
-    .append("div")
-    .attr("class", "bar")
-    .style("height", function(d) {
-        var barHeight = d * 5;
-        return barHeight + "px";
-    });
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr("height" , h);
+
+// d3.select("body").selectAll("div")
+//     .data(dataset)
+//     .enter()
+//     .append("div")
+//     .attr("class", "bar")
+//     .style("height", function(d) {
+//         var barHeight = d * 5;
+//         return barHeight + "px";
+//     });
+
+svg.selectAll("rect")
+	.data(dataset)
+	.enter()
+	.append("rect")
+	.attr("x", function(d,i){
+		return i * (w/dataset.length);
+	})
+	.attr("y", function(d){
+		return h-d*3;
+	})
+	.attr("width", w/dataset.length - barPadding)
+	.attr("height", h)
+	.attr("margin-right", barPadding);
